0ad8258f33607d249e21e285afd46b71c5ca0f1e
IFAM1 <cbbe.isaifelipe.anguela.ma@unifranz.edu.bo>
2026-08-21 09:31:08 -0400

    feat: configurar base de datos y autenticacion JWT

---
+++ backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local'],
+    }),
+    PrismaModule,
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
